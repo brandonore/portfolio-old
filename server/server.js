18b8ac819eed8c8b145f1e5b3ee3a5562b0216e6
Brandon Ore <y2ktheory@gmail.com>
2018-05-02 17:15:57 -0600

    Removed routes folder

--- server/server.js
+++ server/server.js
@@ -6,7 +6,10 @@ const port = process.env.PORT || 3000;
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const index = require('./routes/index');
+const express = require('express');
+const router = express.Router();
+const nodemailer = require('nodemailer');
+const creds = require('./config/config');
 
 // setup app
 app.use(logger('dev'));
@@ -22,3 +25,50 @@ app.get('*', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is listening on port: ${port}`);
 });
+
+let transport = {
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    requireTLS: true,
+    auth: {
+      user: creds.USER,
+      pass: creds.PASS
+    }
+}
+
+const transporter = nodemailer.createTransport(transport);
+
+transporter.verify((error, success) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Server is ready to take messages');
+    }
+});
+
+router.post('/send', (req, res, next) => {
+    const name = req.body.name
+    const email = req.body.email
+    const message = req.body.message
+    const content = `name: ${name} \n email: ${email} \n message: ${message} `
+  
+    const mail = {
+      from: name,
+      to: 'y2ktheory@gmail.com',
+      subject: 'New Message from Portfolio',
+      text: content
+    }
+  
+    transporter.sendMail(mail, (err, data) => {
+      if (err) {
+        res.json({
+          msg: 'fail'
+        })
+      } else {
+        res.json({
+          msg: 'success'
+        })
+      }
+    })
+  })
