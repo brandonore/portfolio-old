81e3ebb0d5936d9274f66d1bbee1c4b4c2672e6d
Brandon Ore <y2ktheory@gmail.com>
2018-05-02 18:12:49 -0600

    Url change

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -11,7 +11,7 @@ class ContactForm extends React.Component {
 
         axios({
             method: 'POST',
-            url: 'http://localhost:3000/send',
+            url: 'https://rocky-anchorage-67021.herokuapp.com/send',
             data: {
                 name,
                 email,
