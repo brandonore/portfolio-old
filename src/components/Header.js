9c667b98d352af9437cb78a93f5d660a3fccd9f2
Brandon Ore <y2ktheory@gmail.com>
2018-05-17 23:19:27 -0600

    Adjusted media queries for new menu. Cleaned up css

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,19 +5,48 @@ import { slide as Menu } from 'react-burger-menu'
 
 class Header extends React.Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            menuOpen: false
+        }
+    }
+
+    handleStateChange(state) {
+        this.setState({
+            menuOpen: state.isOpen
+        });
+    }
+
+    closeMenu() {
+        this.setState({
+            menuOpen: false
+        });
+    }
+
+    toggleMenu() {
+        this.setState({
+            menuOpen: !this.state.menuOpen
+        });
+    }
+
     showSettings(event) {
         event.preventDefault();
     }
 
     render() {
         return (
-            <Menu className="menu" width='13%'>
+            <Menu 
+                className="menu" 
+                isOpen={this.state.menuOpen} 
+                onStateChange={(state) => this.handleStateChange(state)} 
+                width={window.innerWidth > 767 ? '15%' : '100%'}
+            >
                 <img className="home-img" src={cropped} alt="portfolio image" />
-                <NavLink to="/" activeClassName="is-active" exact={true}><i className="menu-item fal fa-home"></i> Home</NavLink>
-                <NavLink to="/portfolio" activeClassName="is-active" exact={true}><i className="menu-item fal fa-folder-open"></i> Portfolio</NavLink>
-                <NavLink to="/resume" activeClassName="is-active" exact={true}><i className="menu-item fal fa-file-alt"></i> Resume</NavLink>
-                <NavLink to="/contact" activeClassName="is-active"><i className=" menu-itemfal fa-envelope"></i> Contact</NavLink>
-                <a onClick={ this.showSettings } className="menu-item--small" href="">Settings</a>
+                <NavLink to="/" activeClassName="is-active" exact={true} onClick={() => this.closeMenu()}><i className="menu-item fal fa-home"></i> Home</NavLink>
+                <NavLink to="/portfolio" activeClassName="is-active" exact={true} onClick={() => this.closeMenu()}><i className="menu-item fal fa-folder-open"></i> Portfolio</NavLink>
+                <NavLink to="/resume" activeClassName="is-active" exact={true} onClick={() => this.closeMenu()}><i className="menu-item fal fa-file-alt"></i> Resume</NavLink>
+                <NavLink to="/contact" activeClassName="is-active" exact={true} onClick={() => this.closeMenu()}><i className=" menu-item fal fa-envelope"></i> Contact</NavLink>
                 <div className="nav-bottom">
                     <div className="nav-social">
                         <a href="https://github.com/brandonore/"><i class="fab fa-github"></i></a>
