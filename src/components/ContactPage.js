41a36069c7e0cf7bdc897165c8bb167d91f5471b
Brandon Ore <y2ktheory@gmail.com>
2018-06-06 22:03:43 -0600

    Updated portfolio page styles and overlays

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ContactForm from './ContactForm';
+import Particles from 'react-particles-js';
 
 class ContactPage extends React.Component {
 
@@ -8,6 +9,28 @@ class ContactPage extends React.Component {
             <div className="contact-div">
                 <h1>Contact</h1>
                 <ContactForm />
+                <Particles width="90%"
+                    params={{
+                        particles: {
+                            color: {
+                                value: '#262626'
+                            },
+                            number: {
+                                value: 100
+                            },
+                            line_linked: {
+                                opacity: 0.5,
+                                color: '#262626',
+                                width: '1'
+                            }
+                        }
+                    }}
+                    style={{
+                        position: 'fixed',
+                        right: 0,
+                        top: 0,
+                        zIndex: 0
+                }}/>
             </div>
         )
     }
--- src/components/PortfolioPage.js
+++ src/components/PortfolioPage.js
@@ -2,8 +2,11 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import chii from '../../public/imgs/chiibody.png';
 import aurora from '../../public/imgs/auroralogo.png';
+import usd from '../../public/imgs/usd.png';
+import twitter from '../../public/imgs/twitter.png';
 import Lightbox from 'react-image-lightbox';
 import 'react-image-lightbox/style.css';
+import Particles from 'react-particles-js';
 
 let images = ['https://facebook.github.io/react/logo-og.png', 'http://scimg.jb51.net/allimg/160815/103-160Q509544OC.jpg'];
 let captions = ['test1', 'test2'];
@@ -54,14 +57,17 @@ class PortfolioPage extends React.Component {
                                 Click the link icon to the right to view the documentation.
                                 <br/>
                                 <br/>
-                                <i><b>Technologies used:</b></i>&nbsp; javascript & the <a href="https://discord.js.org">Discord.js</a> module.</p>
-                                <div className="overlay"></div>
+                                <i><b>Technologies used:</b></i>&nbsp; Javascript & the <a href="https://discord.js.org">Discord.js</a> module.</p>
+                                <div className="overlay">
+                                    <div className="content-links">
+                                        <div className="clinks-container">
+                                            <a href="https://github.com/brandonore/chii"><i className="fab fa-github"></i></a>
+                                            <i className="fas fa-image" onClick={() => this.setState({ isOpen: true, photoIndex: 0 })}></i>
+                                            <i className="fas fa-link"></i>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div className="content-links">
-                            <a href="https://github.com/brandonore/chii"><i className="fab fa-github"></i></a>
-                            <i className="fas fa-image" onClick={() => this.setState({ isOpen: true, photoIndex: 0 })}></i>
-                            <i className="fas fa-link"></i>
                         </div>
                     </div>
                         
@@ -74,18 +80,88 @@ class PortfolioPage extends React.Component {
                                 View the github for a detailed readme. Download available for Windows and macOS.
                                 <br/>
                                 <br/>
-                                <i><b>Technologies used:</b></i>&nbsp; javascript, <a href="https://electronjs.org">Electron</a> & <a href="https://nodejs.org">Node JS</a></p>
-                                <div className="overlay"></div>
+                                <i><b>Technologies used:</b></i>&nbsp; Javascript, <a href="https://electronjs.org">Electron</a> & <a href="https://nodejs.org">Node JS</a></p>
+                                <div className="overlay">
+                                    <div className="content-links">
+                                        <div className="clinks-container">
+                                            <a href="https://github.com/brandonore/aurora"><i className="fab fa-github"></i></a>
+                                            <i className="fas fa-image" onClick={() => this.setState({ isOpen: true, photoIndex: 1 })}></i>
+                                            <a href="https://github.com/brandonore/aurora/releases"><i className="fas fa-download"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div className="content-links">
-                            <a href="https://github.com/brandonore/aurora"><i className="fab fa-github"></i></a>
-                            <i className="fas fa-image" onClick={() => this.setState({ isOpen: true, photoIndex: 1 })}></i>
-                            <a href="https://github.com/brandonore/aurora/releases"><i className="fas fa-download"></i></a>
                         </div>
                     </div>
                     {/*=================================================== */}
+                    <div className="custom-row">
+                        <div className="custom-column">
+                            <h2>Expensify</h2>
+                            <div className="content">
+                                <img src={usd} alt="usd logo" className="usd-logo" />
+                                <p>Expense manager built with React. Features Date/Price sorting and a filter search.
+                                Uses firebase with Google Authentication to keep track of individual users.
+                                <br/>
+                                <br/>
+                                <i><b>Technologies used:</b></i>&nbsp; Javascript, <a href="https://reactjs.org/">React</a>, <a href="https://redux.js.org/">Redux</a> 
+                                &nbsp;& <a href="https://firebase.google.com/">Firebase</a></p>
+                                <div className="overlay">
+                                    <div className="content-links">
+                                        <div className="clinks-container">
+                                            <a href="https://github.com/brandonore/chii"><i className="fab fa-github"></i></a>
+                                            <i className="fas fa-image" onClick={() => this.setState({ isOpen: true, photoIndex: 0 })}></i>
+                                            <i className="fas fa-link"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="custom-row">
+                        <div className="custom-column">
+                            <h2>Twitter Interface</h2>
+                            <div className="content">
+                                <img src={twitter} alt="twitter logo" className="twitter-logo" />
+                                <p>A custom interface built for twitter. Displays recent tweets, friends and direct messages.
+                                Requires a separate config file to use. Please see the documentation. 
+                                <br/>
+                                <br/>
+                                <i><b>Technologies used:</b></i>&nbsp; Javascript, <a href="https://nodejs.org">Node JS</a> & <a href="https://expressjs.com/">Express</a></p>
+                                <div className="overlay">
+                                    <div className="content-links">
+                                        <div className="clinks-container">
+                                            <a href="https://github.com/brandonore/chii"><i className="fab fa-github"></i></a>
+                                            <i className="fas fa-image" onClick={() => this.setState({ isOpen: true, photoIndex: 0 })}></i>
+                                            <i className="fas fa-link"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+                <Particles width="90%"
+                    params={{
+                        particles: {
+                            color: {
+                                value: '262626'
+                            },
+                            number: {
+                                value: 100
+                            },
+                            line_linked: {
+                                opacity: 0.5,
+                                color: '#262626',
+                                width: '1'
+                            }
+                        }
+                    }}
+                    style={{
+                        position: 'fixed',
+                        right: 0,
+                        top: 0,
+                        zIndex: 0
+                }}/>
             </div>
         )
     }
