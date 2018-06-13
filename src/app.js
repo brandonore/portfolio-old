1a6894e6f03ff6cc8063aa0f591dd72ee29fc055
Brandon Ore <y2ktheory@gmail.com>
2018-06-12 22:32:50 -0600

    Updated styles and svg icons

--- src/app.js
+++ src/app.js
@@ -4,10 +4,13 @@ import AppRouter from './routers/AppRouter';
 import fontawesome from '@fortawesome/fontawesome'
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import light from '@fortawesome/fontawesome-pro-light';
+import solid from '@fortawesome/fontawesome-pro-solid';
+import reg from '@fortawesome/fontawesome-pro-regular';
+import brands from '@fortawesome/fontawesome-free-brands';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-fontawesome.library.add(light);
+fontawesome.library.add(light, solid, reg, brands);
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 import cropped from '../../public/imgs/cropped.png';
 import $ from 'jquery';
 import '../../node_modules/bootstrap/dist/js/bootstrap';
@@ -18,19 +19,10 @@ class Header extends React.Component {
                     <i className="fas fa-bars fa-2x toggle-btn" data-toggle="collapse" data-target="#menu-content"></i>
                     <div className="menu-list">
                         <ul id="menu-content" className="menu-content collapse out">
-                            <NavLink to="/" activeClassName="is-active" exact={true}><i className="fal fa-home"></i>Home</NavLink>
-                            <NavLink to="/portfolio" activeClassName="is-active" exact={true}><i className="fal fa-folder"></i>Portfolio</NavLink>
-                            <NavLink to="/resume" activeClassName="is-active" exact={true}><i className="fal fa-file-pdf"></i>Resume</NavLink>
-                            <NavLink to="/contact" activeClassName="is-active" exact={true}><i className="fal fa-envelope"></i>Contact</NavLink> 
-                        </ul>
-                    </div>
-                    <div className="github-content">
-                        <div className="github-title">Github activity</div>
-                        <ul className="github-container">
-                            <li>Repos &nbsp;<span>|</span> &nbsp;<b>14</b></li>
-                            <li>Followers &nbsp;<span>|</span> &nbsp;<b>2</b></li>
-                            <li>Stars &nbsp;<span>|</span> &nbsp;<b>15</b></li>
-                            <li>Following &nbsp;<span>|</span> &nbsp;<b>12</b></li>
+                            <NavLink to="/" activeClassName="is-active" exact={true}><FontAwesomeIcon icon={['fal', 'home']} />Home</NavLink>
+                            <NavLink to="/portfolio" activeClassName="is-active" exact={true}><FontAwesomeIcon icon={['fal', 'folder']} />Portfolio</NavLink>
+                            <NavLink to="/resume" activeClassName="is-active" exact={true}><FontAwesomeIcon icon={['fal', 'file-pdf']} />Resume</NavLink>
+                            <NavLink to="/contact" activeClassName="is-active" exact={true}><FontAwesomeIcon icon={['fal', 'envelope']} />Contact</NavLink> 
                         </ul>
                     </div>
                     <div className="trend">
@@ -41,12 +33,12 @@ class Header extends React.Component {
                     </div>
                     <div className="nav-bottom">
                         <div className="nav-social">
-                            <a href="https://github.com/brandonore/"><i className="fab fa-github"></i></a>
-                            <a href="https://twitter.com/brandon_ore/"><i className="fab fa-twitter"></i></a> 
-                            <a href="https://instagram.com/y2ktheory/"><i className="fab fa-instagram"></i></a>
+                            <a href="https://github.com/brandonore/"><FontAwesomeIcon icon={['fab', 'github']} /></a>
+                            <a href="https://twitter.com/brandon_ore/"><FontAwesomeIcon icon={['fab', 'twitter']} /></a> 
+                            <a href="https://instagram.com/y2ktheory/"><FontAwesomeIcon icon={['fab', 'instagram']} /></a>
                         </div>
                         <div className="nav-footer">
-                            <i className="fal fa-copyright"></i> 2018 Brandon Ore. Built with React <a href="https://reactjs.org/"><i className="fab fa-react"></i></a>
+                        <FontAwesomeIcon icon={['fal', 'copyright']} /> 2018 Brandon Ore. Built with React <a href="https://reactjs.org/"><FontAwesomeIcon icon={['fab', 'react']} /></a>
                         </div>
                     </div>
                 </div>
